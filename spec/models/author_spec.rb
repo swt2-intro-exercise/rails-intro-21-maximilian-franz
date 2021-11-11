46457ee88935ce893c30dc5c7af24fb4e8713959
Maximilian Franz <maximilian.franz@student.hpi.de>
2021-11-11 20:09:42 +0100

    add test for author model

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,14 @@
+describe "Author", type: :model do
+    it "should have text inputs for an author's first name, last name, and homepage" do
+      author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "https://en.wikipedia.org/wiki/Alan_Turing")
+      # these are the standard names given to inputs by the Rails form builder
+      expect(author.first_name).to eq("Alan")
+      expect(author.last_name).to eq("Turing")
+      expect(author.homepage).to eq("https://en.wikipedia.org/wiki/Alan_Turing")
+    end
+    it "should have a method 'name' that returns its full name" do
+      author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "https://en.wikipedia.org/wiki/Alan_Turing")
+      expect(author.name).to eq("Alan Turing")
+    end
+  end
+  
