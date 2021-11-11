e9fb6665e2b33a649dd65e6a3c58069952d3df87
Maximilian Franz <maximilian.franz@student.hpi.de>
2021-11-11 21:37:34 +0100

    add test to ensure author has last name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 
 describe "Author", type: :model do
     it "should have text inputs for an author's first name, last name, and homepage" do
-      author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "https://en.wikipedia.org/wiki/Alan_Turing")
+      author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
       # these are the standard names given to inputs by the Rails form builder
       expect(author.first_name).to eq("Alan")
       expect(author.last_name).to eq("Turing")
-      expect(author.homepage).to eq("https://en.wikipedia.org/wiki/Alan_Turing")
+      expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
     end
     it "should have a method 'name' that returns its full name" do
-      author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "https://en.wikipedia.org/wiki/Alan_Turing")
+      author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
       expect(author.name).to eq("Alan Turing")
+    end
+    it "should have a last name" do
+      author = Author.new(first_name: "Alan", last_name: nil, homepage: "http://wikipedia.org/Alan_Turing")
+      expect(author).not_to be_valid
     end
   end
   
