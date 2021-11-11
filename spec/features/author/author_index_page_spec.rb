732198a4065030285e845da61f37c8ad55850c2e
Maximilian Franz <maximilian.franz@student.hpi.de>
2021-11-11 21:44:36 +0100

    add existence test

--- spec/features/author/author_index_page_spec.rb
+++ spec/features/author/author_index_page_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe "Author index page", type: :feature do
+    it "should exist at 'authors_path' and render withour error" do
+        # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+        visit authors_path
+    end
     it "should display all saved authors" do
         visit authors_path
         Author.all do |author|
--- spec/features/author/author_show_page_spec.rb
+++ spec/features/author/author_show_page_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe "Show author page", type: :feature do
+    it "should exist at 'author_path(author)' and render withour error" do
+        # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+        @alan = FactoryBot.create :author
+        visit author_path(@alan)
+    end
     it "should display the authors details" do
         @alan = FactoryBot.create :author
         visit author_path(@alan)
