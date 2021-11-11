4cc02734a461bed462ca0aab72581039b0853b3a
Maximilian Franz <maximilian.franz@student.hpi.de>
2021-11-11 21:22:28 +0100

    add author index page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,6 +12,10 @@ class AuthorsController < ApplicationController
     def show
         @author = Author.find(params[:id])
     end
+
+    def index
+        @authors = Author.all
+    end
     
     private
         def author_params
