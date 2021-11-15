d79970ca3c5ff26005e00ec36011b8146af8200b
Maximilian Franz <maximilian.franz@student.hpi.de>
2021-11-15 10:09:10 +0100

    fix typo

--- spec/features/paper/show_paper_page_spec.rb
+++ spec/features/paper/show_paper_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Show paper page", type: :feature do
     it "should exist at 'paper_path(paper)' and render withour error" do
         # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
-        @peper = FactoryBot.create :paper
+        @paper = FactoryBot.create :paper
         visit paper_path(@paper)
     end
     it "should display the papers authors name" do
